2d9fe4a1676f47ea68d82e180ff691d2a41d2a9c
sakshi tiwari <78548279+sakshitiwari-prog@users.noreply.github.com>
2024-04-13 16:43:53 +0530

    Delete client directory

--- client/server/routes/visual-routes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const visualRouter = express.Router();
-const {getAllVisualData} = require("../controllers/visual-controller")
-visualRouter.get("/", getAllVisualData);
-
-module.exports = visualRouter;
